b028f9a68d527ddf30aadc091420f791449c482a
DirkieDurky <dirk@freijters.nl>
2025-03-11 14:38:31 +0100

    Add links to css and js files
    Also make language dynamic
    This also makes the page auto-reload when making changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>BlendBarometer</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
