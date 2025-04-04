205666c92ab3635af5878917ca788c19111be2f4
DirkieDurky <dirk@freijters.nl>
2025-04-04 08:22:42 +0200

    Hide empty scroll bars

--- resources/views/components/progress-step.blade.php
+++ resources/views/components/progress-step.blade.php
@@ -4,7 +4,7 @@
             <aside class="d-flex flex-column flex-shrink-0 p-4 bg-white" style="width: 280px;">
                 <h1>Zijbalk</h1>
             </aside>
-            <div class="content flex-grow-1 px-5 py-4 h-100 overflow-scroll">
+            <div class="content flex-grow-1 px-5 py-4 h-100 overflow-auto">
                 {{ $slot }}
             </div>
         </div>
