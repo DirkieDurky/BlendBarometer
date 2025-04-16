c6a8b3e1f5e37fbdb61ce2e795bcaa887800f6d1
pepijnemmers <pepijn.emmers@hotmail.com>
2025-04-16 13:58:33 +0200

    fix: replace divs with spans for better semantic structure

--- resources/views/components/lesson-question-component.blade.php
+++ resources/views/components/lesson-question-component.blade.php
@@ -9,20 +9,20 @@
     <div class="d-flex gap-4" role="group">
         <input type="radio" class="form-check-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_nooit" value="0" autocomplete="off" {{ $selectedAnswer === '0' ? 'checked' : '' }} required>
         <label class="form-check-label border-2 border rounded shadow-sm flex-fill py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_nooit">
-            <div style="font-size: 2rem;">🫢</div>
-            <div class="mt-2 text-nowrap">Nooit</div>
+            <span style="font-size: 2rem;">🫢</span>
+            <span class="mt-2 text-nowrap">Nooit</span>
         </label>
 
         <input type="radio" class="form-check-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_af_en_toe" value="1" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
         <label class="form-check-label border-2 border rounded shadow-sm flex-fill py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_af_en_toe">
-            <div style="font-size: 2rem;">🙂</div>
-            <div class="mt-2 text-nowrap">Af en toe</div>
+            <span style="font-size: 2rem;">🙂</span>
+            <span class="mt-2 text-nowrap">Af en toe</span>
         </label>
 
         <input type="radio" class="form-check-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_vaak" value="2" autocomplete="off" {{ $selectedAnswer === '2' ? 'checked' : '' }} required>
         <label class="form-check-label border-2 border rounded shadow-sm flex-fill py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_vaak">
-            <div style="font-size: 2rem;">😃</div>
-            <div class="mt-2 text-nowrap">Vaak</div>
+            <span style="font-size: 2rem;">😃</span>
+            <span class="mt-2 text-nowrap">Vaak</span>
         </label>
     </div>
 </div>
