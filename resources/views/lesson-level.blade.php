4ae9148f16172d81fd9262fd005d1bca6ce9a532
pepijnemmers <pepijn.emmers@hotmail.com>
2025-04-16 14:36:49 +0200

    added favicon and improve button styles

--- resources/views/lesson-level.blade.php
+++ resources/views/lesson-level.blade.php
@@ -7,11 +7,12 @@
 
     <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
-            <h5 class="text-muted mb-1">{{ $subCategory->id }} van {{ $totalSteps }} - {{ $subCategory->QuestionCategory->name }}</h5>
+            <h5 class="text-muted mb-1">{{ $subCategory->id }} van {{ $totalSteps }}
+                - {{ $subCategory->QuestionCategory->name }}</h5>
             <h3 class="fw-bold mb-1">{{ $subCategory->name }}</h3>
             <p class="text-muted">Hoe vaak gebruik je ...</p>
         </div>
-        {{-- <button class="btn btn-secondary btn-sm">Hulp nodig?</button> verwijst naar tussenpagina --}}
+        {{-- <button class="btn btn-secondary">Hulp nodig?</button> // TODO: redirect to 'tussenpagina' --}}
     </div>
 
     <form method="POST" action="{{ route('lesson-level.submit', $currentStep) }}">
@@ -23,11 +24,11 @@
                 $selectedAnswer = $answers[$currentStep][$question->id] ?? null;
                 $description = $question->description ?? null;
             @endphp
-            <x-lesson-question-component 
-            :question="$question" 
-            :selectedAnswer="$selectedAnswer" 
-            :fieldName="$fieldName"
-            :description="$description" />
+            <x-lesson-question-component
+                :question="$question"
+                :selectedAnswer="$selectedAnswer"
+                :fieldName="$fieldName"
+                :description="$description"/>
         @endforeach
 
         <div id="custom-question-container">
@@ -38,23 +39,25 @@
                         $selectedAnswer = $answers[$currentStep][$key] ?? null;
                         $questionText = str_replace('custom_question_', '', $key);
                     @endphp
-                    <x-lesson-question-component 
-                    :question="(object) ['id' => $key, 'text' => $questionText]" 
-                    :selectedAnswer="$selectedAnswer" 
-                    :fieldName="$fieldName" />
+                    <x-lesson-question-component
+                        :question="(object) ['id' => $key, 'text' => $questionText]"
+                        :selectedAnswer="$selectedAnswer"
+                        :fieldName="$fieldName"/>
                 @endforeach
             @endif
         </div>
 
         <div class="mb-5">
-            <label class="form-label"><strong>Gebruik je iets in de categorie {{ $subCategory->name }}, wat niet voorbij gekomen is?</strong></label>
+            <label class="form-label"><strong>Gebruik je iets in de categorie {{ $subCategory->name }}, wat niet voorbij
+                    gekomen is?</strong></label>
             <div class="input-group">
-                <input type="text" class="form-control" id="custom_input" name="custom_input" placeholder="Vul je vraag in">
+                <input type="text" class="form-control" id="custom_input" name="custom_input"
+                       placeholder="Vul je vraag in">
                 <button class="btn btn-primary" type="button" id="addCustomQuestionBtn">Toevoegen</button>
             </div>
         </div>
 
-        <x-navigation-buttons-with-submit :previous="route('lesson-level.previous', $currentStep)" />
+        <x-navigation-buttons-with-submit :previous="route('lesson-level.previous', $currentStep)"/>
     </form>
     <script src="{{ asset('js/customQuestion.js') }}"></script>
     <script>
