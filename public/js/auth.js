c3da1db0614442874574f1f893b7dde0d7c2851c
Kevin <k.janssen11@student.avans.nl>
2025-04-17 10:42:37 +0200

    code input javascript

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,3 +1,31 @@
 
 const inputs = document.querySelectorAll('input.rectangle');
 
+inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        if (input.value.length === 1 && index < inputs.length - 1)
+        {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && input.value.length === 0 && index > 0)
+        {
+            inputs[index - 1].focus();
+        }
+    });
+
+    input.addEventListener('paste', (e) => {
+        e.preventDefault();
+        const paste = e.clipboardData.getData('text');
+
+        const reg = /\D/g;
+        const digits = paste.replace(reg, '');
+
+        for (i = 0; i < inputs.length && i < digits.length; i++)
+        {
+            inputs[i].value = digits[i];
+        }
+    });
+});
