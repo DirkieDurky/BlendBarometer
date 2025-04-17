373d35bcecd8ca3e2c701a488f9de289b0b498ca
MatthijsCr <msj.cromwijk@student.avans.nl>
2025-04-17 14:42:53 +0200

    img added

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers;
 
 use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\SimpleType\Jc;
 
 class ReportController extends Controller{
 
     public function sendReport(){
         $phpWord = new PhpWord();
+        $fileName = 'Rapport ' . session('module') . '.docx';
 
         $section = $phpWord->addSection();
-        $section->addText('Hello World from Laravel + PHPWord!');
+
+        $section->addText('Hello World from Laravel + PHPWord!',['alignment' => Jc::CENTER]);
+        $section->addTextBreak(1);
+        $section->addImage(public_path('images/introduction_image.png'), 
+        ['alignment' => Jc::CENTER,
+        'width' => 450, 
+        'height' => 450, 
+        ]);
+
 
         $writer = IOFactory::createWriter($phpWord, 'Word2007');
 
-        $fileName = 'hello-world.docx';
         $tempFile = tempnam(sys_get_temp_dir(), $fileName);
         $writer->save($tempFile);
 
