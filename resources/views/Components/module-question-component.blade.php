a9f0db03641f6a6a3a6a1830fb37fff83fcd756b
DirkieDurky <dirk@freijters.nl>
2025-04-18 17:46:00 +0200

    Answers are now saved in the session

--- resources/views/Components/module-question-component.blade.php
+++ resources/views/Components/module-question-component.blade.php
@@ -10,22 +10,22 @@
         @endif
     </div>
     <div class="col py-4 d-flex flex-column justify-content-center align-items-center">
-        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_explore" value="0" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
+        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_explore" value="0" autocomplete="off" {{ $selectedAnswer === '0' ? 'checked' : '' }} required>
         <label class="module-label border-2 border rounded-circle shadow-sm col py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_explore" style="width:50px; height:50px; max-height:50px">
         </label>
     </div>
     <div class="col py-4 d-flex flex-column justify-content-center align-items-center">
-        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_apply" value="0" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
+        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_apply" value="1" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
         <label class="module-label border-2 border rounded-circle shadow-sm col py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_apply" style="width:50px; height:50px; max-height:50px">
         </label>
     </div>
     <div class="col py-4 d-flex flex-column justify-content-center align-items-center">
-        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_plan" value="0" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
+        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_plan" value="2" autocomplete="off" {{ $selectedAnswer === '2' ? 'checked' : '' }} required>
         <label class="module-label border-2 border rounded-circle shadow-sm col py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_plan" style="width:50px; height:50px; max-height:50px">
         </label>
     </div>
     <div class="col py-4 d-flex flex-column justify-content-center align-items-center">
-        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_anchored" value="0" autocomplete="off" {{ $selectedAnswer === '1' ? 'checked' : '' }} required>
+        <input type="radio" class="module-input visually-hidden" name="{{ $fieldName }}" id="{{ $fieldName }}_anchored" value="3" autocomplete="off" {{ $selectedAnswer === '3' ? 'checked' : '' }} required>
         <label class="module-label border-2 border rounded-circle shadow-sm col py-4 d-flex flex-column justify-content-center align-items-center" for="{{ $fieldName }}_anchored" style="width:50px; height:50px; max-height:50px">
         </label>
     </div>
