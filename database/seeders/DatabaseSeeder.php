4c6cca0d851ff59b807826e85f8e682b86ba917b
DirkieDurky <dirk@freijters.nl>
2025-04-18 19:46:16 +0200

    Add all seeders with proper capitalization

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        $this->call(ContentSeeder::class);
+        $this->call(QuestionSeeder::class);
+        $this->call(AcademySeeder::class);
+        $this->call(FormSectionSeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(QuestionCategorySeeder::class);
+        $this->call(UserSeeder::class);
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
---
+++ database/seeders/QuestionCategorySeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Question_category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class QuestionCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Question_category::query()->delete();
+        $categories = [
+            [
+                'id' => 1,
+                'form_section_id' => 1,
+                'name' => 'Fysieke leeractiviteiten',
+                'description' => null,
+            ],
+            [
+                'id' => 2,
+                'form_section_id' => 1,
+                'name' => 'Online leeractiviteiten',
+                'description' => null,
+            ],
+            [
+                'id' => 3,
+                'form_section_id' => 2,
+                'name' => 'Samenhangend',
+                'description' => 'Context van de module',
+            ],
+            [
+                'id' => 4,
+                'form_section_id' => 2,
+                'name' => 'Organisatorisch',
+                'description' => 'Waar en wanneer vindt het onderwijs plaats',
+            ],
+            [
+                'id' => 5,
+                'form_section_id' => 2,
+                'name' => 'Didactisch',
+                'description' => 'Inrichten van de leeractiviteiten',
+            ],
+        ];
+
+        Question_category::insert($categories);
+    }
+}
