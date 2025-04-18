3335d04eb46482d29a3ea0b4cd050996b37e3b10
MatthijsCr <msj.cromwijk@student.avans.nl>
2025-04-18 10:12:17 +0200

    logo's toegevoegt maar nog niet goed gestyled

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,18 +6,46 @@ use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\IOFactory;
 use PhpOffice\PhpWord\SimpleType\Jc;
 
-class ReportController extends Controller{
+class ReportController extends Controller
+{
 
-    public function sendReport(){
+    public function sendReport()
+    {
         $phpWord = new PhpWord();
         $fileName = 'Rapport ' . session('module') . '.docx';
 
+        $phpWord->setDefaultFontName('Inter');
+        $phpWord->setDefaultFontSize(12);
+
         $section = $phpWord->addSection();
 
-        $section->addText('Hello World from Laravel + PHPWord!',['alignment' => Jc::CENTER]);
+        $section->addImage(public_path('images/logo-avans-red.png'), [
+            'width' => 100,
+            'height' => 30,
+            'wrappingStyle' => 'inline',
+            'positioning' => 'absolute',
+            'posHorizontalRel' => 'margin',
+            'marginLeft' => -1000,
+            'posVerticalRel' => 'margin',
+        ]);
+        
+        $section->addImage(public_path('images/logo-avans-red.png'), [
+            'width' => 100,
+            'height' => 30,
+            'wrappingStyle' => 'inline',
+            'positioning' => 'absolute',
+            'posHorizontal' => \PhpOffice\PhpWord\Style\Image::POSITION_HORIZONTAL_RIGHT,
+            'posHorizontalRel' => 'page',
+            'posVerticalRel' => 'margin',
+        ]);
+
+        $section->addText('Tussenrapport - '. session('module'),['size' => 35, 'bold' => true],['alignment' => Jc::CENTER]);
+        $section->addText('Blended Learning '. '[datum todo]',['size' => 15],['alignment' => Jc::CENTER]);
+
         $section->addTextBreak(1);
-        $section->addImage(public_path('images/introduction_image.png'), 
-        ['alignment' => Jc::CENTER,
+
+        $section->addImage(public_path('images/introduction_image.png'), [
+        'alignment' => Jc::CENTER,
         'width' => 450, 
         'height' => 450, 
         ]);
