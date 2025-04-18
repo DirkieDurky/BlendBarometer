8f7310d8b0f41e28399ddf8a4ca0d3d319684672
DirkieDurky <dirk@freijters.nl>
2025-04-18 18:45:27 +0200

    Reinstate databaseSeeder

---
+++ database/seeders/databaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        $this->call(ContentSeeder::class);
+        $this->call(QuestionSeeder::class);
+        $this->call(AcademySeeder::class);
+        $this->call(FormSectionSeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(QuestionCategorySeeder::class);
+
+        // User::factory(10)->create();
+        User::query()->delete();
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
