b8c8dc543186c322312c902e62bb52e432e891fb
Wessenator <deheuswessel@gmail.com>
2025-05-09 00:27:52 +0200

    refactor: fix indentation and used blade syntax

--- app/Http/Controllers/IntermediateController.php
+++ app/Http/Controllers/IntermediateController.php
@@ -3,61 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\Content;
-use Illuminate\Http\Request;
 use App\Models\Sub_category;
 use App\Models\Question_category;
 
 class IntermediateController extends Controller
 {
     public function view($sectionName)
-{
-    $content = Content::where('section_name', $sectionName)->firstOrFail();
+    {
+        $content = Content::where('section_name', $sectionName)->firstOrFail();
 
-    $staticContent = [
-        'BlendBarometer' => [
-            'title' => 'Hoe werkt de Blend-Barometer?',
-            'section' => 'Gegevens',
-            'description' => '',
-            'currentStepName' => 'information',
-            'content' => $content,
-            'previous' => route('home'),
-            'next' => route('information'),
-        ],
-        'online en fysieke leeractiviteiten' => [
-            'title' => 'Wat is de volgende stap?',
-            'section' => 'Les niveau',
-            'description' => '',
-            'currentStepName' => 'lessonLevel',
-            'content' => $content,
-            'previous' => route('information'),
-            'next' => route('lesson-level', 1),
-        ],
-        'module niveau' => [
-            'title' => 'Wat is de volgende stap?',
-            'section' => 'Module Niveau',
-            'description' => '',
-            'currentStepName' => 'moduleLevel',
-            'content' => $content,
-            'previous' => route('lesson-level', Sub_category::count()),
-            'next' => route('module-level', 1),
-        ],
-        'overzicht en resultaten' => [
-            'title' => 'Wat is de volgende stap?',
-            'section' => 'Resultaten',
-            'description' => '',
-            'currentStepName' => 'results',
-            'content' => $content,
-            'previous' => route('module-level', Question_category::where('form_section_id', 2)->count()),
-            'next' => route('results'),
-        ],
-    ];
+        $staticContent = [
+            'BlendBarometer' => [
+                'title' => 'Hoe werkt de Blend-Barometer?',
+                'section' => 'Gegevens',
+                'description' => '',
+                'currentStepName' => 'information',
+                'content' => $content,
+                'previous' => route('home'),
+                'next' => route('information'),
+            ],
+            'online en fysieke leeractiviteiten' => [
+                'title' => 'Wat is de volgende stap?',
+                'section' => 'Les niveau',
+                'description' => '',
+                'currentStepName' => 'lessonLevel',
+                'content' => $content,
+                'previous' => route('information'),
+                'next' => route('lesson-level', 1),
+            ],
+            'module niveau' => [
+                'title' => 'Wat is de volgende stap?',
+                'section' => 'Module Niveau',
+                'description' => '',
+                'currentStepName' => 'moduleLevel',
+                'content' => $content,
+                'previous' => route('lesson-level', Sub_category::count()),
+                'next' => route('module-level', 1),
+            ],
+            'overzicht en resultaten' => [
+                'title' => 'Wat is de volgende stap?',
+                'section' => 'Resultaten',
+                'description' => '',
+                'currentStepName' => 'results',
+                'content' => $content,
+                'previous' => route('module-level', Question_category::where('form_section_id', 2)->count()),
+                'next' => route('results'),
+            ],
+        ];
 
-    if (array_key_exists($sectionName, $staticContent)) {
+        if (array_key_exists($sectionName, $staticContent)) {
+            return view('intermediate', $staticContent[$sectionName]);
+        }
 
-        return view('intermediate', $staticContent[$sectionName]);
+        return redirect()->route('home')->with('error', 'Invalid section name.');
     }
-
-    return redirect()->route('home')->with('error', 'Invalid section name.');
-}
-
 }
--- resources/views/intermediate.blade.php
+++ resources/views/intermediate.blade.php
@@ -4,7 +4,7 @@
 
         <div class="mb-3">
             <h1 class="fs-3 fw-bold mb-3">Uitleg {{ $content->section_name }}</h1>
-            <?= $content->info ?>
+            {!! $content->info !!}
         </div>
 
         <x-navigation-buttons :previous="$previous" :next="$next" next-label="Volgende"/>
