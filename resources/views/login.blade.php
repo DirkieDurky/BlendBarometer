c79165e96d747745349e4750d301928a61b63d66
DirkieDurky <dirk@freijters.nl>
2025-05-19 22:54:06 +0200

    Conform to accessibility standards:
    Use placeholders like examples instead of labels
    Use headings where needed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
         <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     @endsection
 
-    <main class="position-relative vh-100">
+    <div class="position-relative vh-100">
         <div class="position-absolute top-0 start-0 w-100">
             <div class="container mt-5">
                 <a href="{{ route('home') }}" class="d-block mb-4">
@@ -11,14 +11,14 @@
                 </a>
             </div>
         </div>
-        <div class="d-flex justify-content-center align-items-center h-100">
+        <main class="d-flex justify-content-center align-items-center h-100">
             <div id="help" class="d-flex flex-column">
-                <h2 class="fw-bold">Verifieer uw e-mail</h2>
+                <h1 class="fw-bold">Verifieer uw e-mail</h1>
                 <p><em>Alleen een @avans.nl e-mail wordt geaccepteerd.</em></p>
                 <form action="{{ route('login.submit') }}" method="POST" id="emailForm">
                     @csrf
                     <label for="email">E-mail</label>
-                    <input id="email" name="email" type="email" class="form-control p-2 mb-2" value="{{ old('email') }}" placeholder="E-mail van gebruiker" value="{{ old('email') }}" required>
+                    <input id="email" name="email" type="email" class="form-control p-2 mb-2" value="{{ old('email') }}" placeholder="bv. voorbeeld@avans.nl" value="{{ old('email') }}" required>
                     @if ($errors->any())
                         @foreach ($errors->all() as $error)
                             <span class="text-danger fw-bold small">{{ $error }}</span>
@@ -27,6 +27,6 @@
                     <button type="submit" class="btn btn-primary mt-3 w-100">Verder</button>
                 </form>
             </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </x-layout>
