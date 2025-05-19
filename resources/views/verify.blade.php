c780182d6fd312537925e98b85c72a0b471c9de7
DirkieDurky <dirk@freijters.nl>
2025-05-19 22:55:00 +0200

    Chance main landmark

--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -6,7 +6,7 @@
         <script src="{{ asset('js/auth.js') }}" defer></script>
     @endsection
 
-    <main class="position-relative vh-100">
+    <div class="position-relative vh-100">
         <div class="position-absolute top-0 start-0 w-100">
             <div class="container mt-5">
                 <a href="{{ route('home') }}" class="d-block mb-4">
@@ -14,9 +14,9 @@
                 </a>
             </div>
         </div>
-        <div class="d-flex justify-content-center align-items-center h-100">
+        <main class="d-flex justify-content-center align-items-center h-100">
             <div class="d-flex flex-column">
-                <h2 class="fw-bold">Verifieer uw e-mail</h2>
+                <h1 class="fw-bold">Verifieer uw e-mail</h1>
                 <p>We hebben een verificatiecode verstuurd <br>naar <strong>{{ session('email') }}</strong>.</p>
                 <form action="{{ route('verify.submit') }}" method="POST" id="codeForm" class="d-flex flex-column">
                     @csrf
@@ -43,8 +43,8 @@
                     </div>
                 </form>
             </div>
-        </div>
-    </main>
+        </main>
+    </div>
     <form action="{{ route('login.submit') }}" method="POST" id="resend">
         @csrf
         <input type="hidden" name="email" value="{{ session('email') }}">
