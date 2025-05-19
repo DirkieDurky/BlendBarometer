8d5021c69542c572cbab50e320c91619a75290aa
DirkieDurky <dirk@freijters.nl>
2025-05-19 22:51:08 +0200

    Prevent pauses in reading main text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
     <main class="container">
         <section class="intro min-vh-100">
             <img src="{{ asset('images/logo.svg') }}" alt="BlendBarometer" class="my-5" />
-            <h1 class="fw-bold mb-4">
-                Een <span class="text-primary">meetinstrument</span> om <br>
-                de kwaliteit van een <br>
+            <h1 class="fw-bold mb-4 w-50">
+                Een <span class="text-primary">meetinstrument</span> om
+                de kwaliteit van een
                 <span class="text-primary">Blended module</span> te meten
             </h1>
             <p class="w-50 mb-4">
