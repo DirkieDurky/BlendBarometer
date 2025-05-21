eda10e31dbd73703bf77333d10c339fd572343b6
Kevin <k.janssen11@student.avans.nl>
2025-05-21 11:23:30 +0200

    validatie

--- resources/views/components/admin/edit-content/home.blade.php
+++ resources/views/components/admin/edit-content/home.blade.php
@@ -3,7 +3,10 @@
 <form action="{{ route('admin.edit-content.home-update') }}" method="POST" class="form w-100 d-flex justify-content-end">
     @csrf
     @method('PUT')
-    <div class="editor mb-3 bg-white">
+    <div class="editor mb-2 bg-white">
         {!! $home !!}
     </div>
 </form>
+@error('content')
+    <span class="text-danger fw-bold small">{{ $message }}</span>
+@enderror
