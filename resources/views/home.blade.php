033d6bdf746d863b159197bd993d799f9ff0ea4d
pepijnemmers <pepijn.emmers@hotmail.com>
2025-06-23 15:07:18 +0200

    font change on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,14 @@
     @endsection
     <main class="container">
         <section class="intro min-vh-100">
-            <img src="{{ asset('images/logo.svg') }}" alt="BlendBarometer" class="my-5" />
+            <div class="d-flex align-items-center justify-content-between py-5">
+                <img src="{{ asset('images/logo.svg') }}" alt="BlendBarometer"/>
+                <button class="btn btn-outline-secondary btn-sm font-dyslexia"
+                        onclick="document.body.classList.toggle('font-dyslexia');">
+                    <i class="bi bi-type me-2"></i>
+                    <span>Dyslexie modus</span>
+                </button>
+            </div>
             <div class="description w-50">
                 {!! str_replace('&nbsp;', ' ', $intro_description) !!}
             </div>
