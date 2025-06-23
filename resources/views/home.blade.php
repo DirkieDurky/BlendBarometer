1413f199c2d1acb8f642506b2ecb21d859072e9a
pepijnemmers <pepijn.emmers@hotmail.com>
2025-06-23 15:27:17 +0200

    Add dyslexia mode toggle and update font settings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
         <section class="intro min-vh-100">
             <div class="d-flex align-items-center justify-content-between py-5">
                 <img src="{{ asset('images/logo.svg') }}" alt="BlendBarometer"/>
-                <button class="btn btn-outline-secondary btn-sm font-dyslexia"
-                        onclick="document.body.classList.toggle('font-dyslexia');">
-                    <i class="bi bi-type me-2"></i>
-                    <span>Dyslexie modus</span>
+                <button class="btn btn-outline-secondary font-dyslexia"
+                        onclick="toggleDyslexiaMode()">
+                    <i class="bi bi-type me-0 me-md-2"></i>
+                    <span class="d-none d-md-inline" style="font-size: 14px;">Dyslexie modus</span>
                 </button>
             </div>
             <div class="description w-50">
