85dc759b1ad7b61d1efe898009f8477cde7386fe
pepijnemmers <pepijn.emmers@hotmail.com>
2025-06-25 12:04:32 +0200

    Update environment settings for Dutch localization and adjust tooltip script placement

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,14 +22,11 @@
     @yield('scripts')
 </head>
 <body>
-
-    {{ $slot }}
-    <script>
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-    </script>
-
+{{ $slot }}
 <script>
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
     function toggleDyslexiaMode() {
         const enabled = localStorage.getItem('dyslexiaMode') === 'enabled';
         localStorage.setItem('dyslexiaMode', enabled ? 'disabled' : 'enabled');
